d16a34803c6234219a9553997fc09b02207b926a
JimHaff <james.haff@generalassemb.ly>
2023-03-20 17:08:44 +0100

    Added performer reference

--- controllers/performers.js
+++ controllers/performers.js
@@ -4,7 +4,36 @@ const Movie = require("../models/movie");
 module.exports = {
   new: newPerformer,
   create,
+  addToCast
 };
+
+function addToCast(req, res){
+
+	// Find the movie from the database
+	// req.params.id variable is defined in the routes
+	// the value of req.params.id is defined 
+	Movie.findById(req.params.id)
+		 .then(function(movieDoc){
+			// add the performers id to the movie's cast array
+			// req.body is the contents of the form, performerId comes from the "name" attribute 
+			// on the select menu in the movies show.ejs
+			movieDoc.cast.push(req.body.performerId);
+			// When mutating a document from the database
+			movieDoc.save()
+					.then(function(){
+						// redirect the user 
+						res.redirect(`/movies/${movieDoc._id}`)
+					}).catch(err => {
+						console.log(err);
+						res.send(err);
+					})
+
+		 }).catch(err => {
+			console.log(err);
+			res.send(err)
+		 })
+	
+}
 
 function create(req, res) {
   // Need to "fix" date formatting to prevent day off by 1
